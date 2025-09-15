0149282cfa9e5378b3ce46c7a3c7a63d95397e62
corentin <corentin@lempire.co>
2025-09-15 16:40:14 +0200

    wip

--- toto.js
+++ toto.js
@@ -12,3 +12,18 @@ try {
     console.error(`Unable to get servers from inventory ${err}`);
     process.exit(1);
   }
+
+  try {
+    // Get front servers list from ansible inventory
+    const doc = yaml.load(fs.readFileSync('infra/ansible/playbooks/lemapp-prod/inventory/hosts.yml', 'utf8'));
+    const frontServers = Object.keys(doc.deploy?.children?.fronts?.hosts);
+    for (const host of frontServers) {
+      await printIPs4(host);
+    }
+    for (const host of frontServers) {
+      await printIPs6(host);
+    }
+  } catch (err) {
+    console.error(`Unable to get servers from inventory ${err}`);
+    process.exit(1);
+  }
